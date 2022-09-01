27295eb49eccd3ca0c1d1decf39703032911701e
Ankit Gupta <ankit.gupta@quokkalabs.com>
2022-09-01 23:55:12 +0530

    design changed:

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,57 +3,89 @@
 @section('content')
 <div class="container">
     @if (session()->has('success'))
-        <div class="alert alert-success" role="alert">
-            {{ session()->get('success') }}
-        </div>
+    <div class="alert alert-success" role="alert">
+        {{ session()->get('success') }}
+    </div>
     @elseif(session()->has('error'))
-        <div class="alert alert-danger" role="alert">
-            {{ session()->get('error') }}
-        </div>
+    <div class="alert alert-danger" role="alert">
+        {{ session()->get('error') }}
+    </div>
     @elseif(session()->has('warning'))
-        <div class="alert alert-warning" role="alert">
-            {{ session()->get('warning') }}
-        </div>
+    <div class="alert alert-warning" role="alert">
+        {{ session()->get('warning') }}
+    </div>
     @endif
-    
-    <section >
+
+    @if(!Auth::check())
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="height: 80vh;">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" style="height: 80vh;" src="https://insidearabia.com/wp-content/uploads/2020/05/IMG_2393.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" style="height: 80vh;" src="https://www.unicef.org/southafrica/sites/unicef.org.southafrica/files/styles/hero_mobile/public/ZAF-BEC9590.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" style="height: 80vh;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAHEuoZjZXX3WQBA7HN8pS81kg4jgZSq0LOg&usqp=CAU" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    @endif
+
+    <section>
         <div class="container py-5">
+            @if(count($allActiveRequest))
+            <h1> Featured Fundraisers</h1>
+            @endif
             <div class="row">
                 @foreach($allActiveRequest as $key => $aAR)
-                <div class="col-md-12 col-lg-4 mb-4 mb-lg-0 rounded-2">
+                <div class="col-md-12 col-lg-4 mb-4 mb-lg-0 rounded-2 mt-4">
                     <div class="card">
                         <div class="d-flex justify-content-between p-3 mb-0">
                             <p class="lead mb-0">{{ $aAR->name }}</p>
                             @switch($aAR->priority)
-                                @case(0)
-                                    <div class="bg-light rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
-                                        <p class="text-white mb-0 small"><i class="fa-solid fa-angles-down"></i> Lowest</p>
-                                    </div>
-                                    @break
-                                    
-                                @case(1)
-                                    <div class="bg-secondary rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
-                                        <p class="text-white mb-0 small"><i class="fa-solid fa-angle-down"></i> Low</p>
-                                    </div>
-                                    @break
-                                    
-                                @case(2)
-                                    <div class="bg-success rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
-                                        <p class="text-white mb-0 small">- Medium</p>
-                                    </div>
-                                    @break
-                                    
-                                @case(3)
-                                    <div class="bg-warning rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
-                                        <p class="text-white mb-0 small"><i class="fa-solid fa-angle-up"></i> High</p>
-                                    </div>
-                                    @break
+                            @case(0)
+                            <div class="bg-light rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
+                                <p class="text-white mb-0 small"><i class="fa-solid fa-angles-down"></i> Lowest</p>
+                            </div>
+                            @break
 
-                                @case(4)
-                                    <div class="bg-danger rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
-                                        <p class="text-white mb-0 small"><i class="fa-solid fa-angles-up"></i> Highest</p>
-                                    </div>
-                                    @break
+                            @case(1)
+                            <div class="bg-secondary rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
+                                <p class="text-white mb-0 small"><i class="fa-solid fa-angle-down"></i> Low</p>
+                            </div>
+                            @break
+
+                            @case(2)
+                            <div class="bg-success rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
+                                <p class="text-white mb-0 small">- Medium</p>
+                            </div>
+                            @break
+
+                            @case(3)
+                            <div class="bg-warning rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
+                                <p class="text-white mb-0 small"><i class="fa-solid fa-angle-up"></i> High</p>
+                            </div>
+                            @break
+
+                            @case(4)
+                            <div class="bg-danger rounded d-flex align-items-center justify-content-center shadow-1-strong p-2" style="width: auto; height: 35px;">
+                                <p class="text-white mb-0 small"><i class="fa-solid fa-angles-up"></i> Highest</p>
+                            </div>
+                            @break
                             @endswitch
                         </div>
                         <div class="card-body">
@@ -84,15 +116,16 @@
 @endsection
 @section('styles')
 <script>
-    $(".alert").fadeTo(2000, 500).slideUp(500, function(){
+    $(".alert").fadeTo(2000, 500).slideUp(500, function() {
         $(".alert").slideUp(500);
     });
-    function showModal(id){
-        $("#acceptAmount"+id).modal('show')
+
+    function showModal(id) {
+        $("#acceptAmount" + id).modal('show')
     }
 
-    const closeModal = (id) =>{
-        $("#acceptAmount"+id).modal('hide')
+    const closeModal = (id) => {
+        $("#acceptAmount" + id).modal('hide')
     }
 </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,19 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" /><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <style>
+        body{
+            background-color: #f0f0f0;
+        }
+    </style>
 </head>
 
 <body>
@@ -56,6 +67,9 @@
                         @if (Route::has('login'))
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                         @endif
                         @else
